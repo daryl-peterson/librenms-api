5ab13da527aa80adc205c3e208435c7390af71e2
daryl-peterson <daryl.peterson@gmail.com>
2023-08-22 10:44:33 -0500

    Update

--- src/Component.php
+++ src/Component.php
@@ -3,13 +3,15 @@
 namespace LibrenmsApiClient;
 
 /**
- * LibreNMS API Health.
+ * LibreNMS API Component.
  *
  * @author      Daryl Peterson <@gmail.com>
  * @copyright   Copyright (c) 2023, Daryl Peterson
  * @license     https://www.gnu.org/licenses/gpl-3.0.txt
  *
  * @since       0.0.1
+ *
+ * @todo Finish class
  */
 class Component
 {
