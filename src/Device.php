9c7f0e9198d6ed3d802a5de0d502e1efdd85a610
daryl-peterson <daryl.peterson@gmail.com>
2023-08-22 10:28:44 -0500

    Update

--- src/Device.php
+++ src/Device.php
@@ -9,7 +9,7 @@ namespace LibrenmsApiClient;
  * @copyright   Copyright (c) 2023, Daryl Peterson
  * @license     https://www.gnu.org/licenses/gpl-3.0.txt
  *
- * @since       0.0.2
+ * @since       0.0.3
  */
 class Device
 {
