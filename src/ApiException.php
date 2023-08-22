c47875e0863401d7fc59af95d5e4f6a3fc711fe7
daryl-peterson <daryl.peterson@gmail.com>
2023-08-22 10:43:10 -0500

    Update

--- src/ApiException.php
+++ src/ApiException.php
@@ -3,7 +3,7 @@
 namespace LibrenmsApiClient;
 
 /**
- * Class description.
+ * LibreNMS API Exception.
  *
  * @category
  *
@@ -11,7 +11,7 @@ namespace LibrenmsApiClient;
  * @copyright   Copyright (c) 2023, Daryl Peterson
  * @license     https://www.gnu.org/licenses/gpl-3.0.txt
  *
- * @since       0.0.1
+ * @since       0.0.2
  */
 class ApiException extends \Exception
 {
